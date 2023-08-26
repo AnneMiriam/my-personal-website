cefa8e70b114f41219d309b701c798f021b95f2f
Anne Hastings <anne.miriam@gmail.com>
2023-08-25 19:28:03 -0600

    added cat content, updated style

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Best Cat Breeds";
 document.querySelector("body").appendChild(h2);
+
